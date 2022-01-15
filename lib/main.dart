650e8d62ed025790a3cbc212df5ac7bdd18f756b
AbdelrahmanElghoul <aelghoul0@gmail.com>
2022-01-15 14:56:37 +0200

    making value 3 instad of 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //1
+    //3
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
