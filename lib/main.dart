1ebc00f78992f83cddc1325f237d1aef76f12889
AbdelrahmanElghoul <aelghoul0@gmail.com>
2022-01-15 15:01:38 +0200

    making value 5 to check how long the message can be. this is just a dummy text used for test to see if the command would refuse message length and how it will apear in commit and log history

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //4
+    //5
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
